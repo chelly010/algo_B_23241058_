0a50775dd4e0c620cc725aa6708537cb1fba3ed1
chelly010 <146046103+chelly010@users.noreply.github.com>
2023-11-28 12:33:11 +0800

    Add files via upload

---
+++ 02-TipeData/src/App.java
@@ -0,0 +1,73 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // tipe datadi java
+        // integer, byte, short, long, double, float, char, boolean
+
+        // integer (satuan)
+        int i = 10;
+        System.out.println("====INTEGER====");
+        System.out.println("nilai integer i = " + i);
+        System.out.println("Nilai MAX       : " + Integer.MAX_VALUE);
+        System.out.println("Nilai MIN       : " + Integer.MIN_VALUE);
+        System.out.println("Besar Integer   : " + Integer.SIZE);
+
+        //byte
+        byte b = 10;
+        System.out.println("====BYTE====");
+        System.out.println("nilai byte b    = " + b);
+        System.out.println("Nilai MAX       : " + Byte.MAX_VALUE);
+        System.out.println("Nilai MIN       : " + Byte.MIN_VALUE);
+        System.out.println("Besar Byte      : " + Byte.SIZE);
+
+        //short
+        short s = 10;
+        System.out.println("====SHORT====");
+        System.out.println("nilai Short s   = " + s);
+        System.out.println("Nilai MAX       : " + Short.MAX_VALUE);
+        System.out.println("Nilai MIN       : " + Short.MIN_VALUE);
+        System.out.println("Besar Short     : " + Short.SIZE);
+
+        //long
+        long l = 10;
+        System.out.println("====LONG===");
+        System.out.println("nilai Long s    = " + s);
+        System.out.println("Nilai MAX       : " + Long.MAX_VALUE);
+        System.out.println("Nilai MIN       : " + Long.MIN_VALUE);
+        System.out.println("Besar Long      : " + Long.SIZE);
+
+        //double
+        long d = 10;
+        System.out.println("====DOUBLE===");
+        System.out.println("nilai Double D  = " + d);
+        System.out.println("Nilai MAX       : " + Double.MAX_VALUE);
+        System.out.println("Nilai MIN       : " + Double.MIN_VALUE);
+        System.out.println("Besar Double    : " + Double.SIZE);
+
+        //Float
+        float f = 10;
+        System.out.println("====FLOAT===");
+        System.out.println("nilai Float f   = " + f);
+        System.out.println("Nilai MAX       : " + Float.MAX_VALUE);
+        System.out.println("Nilai MIN       : " + Float.MIN_VALUE);
+        System.out.println("Besar Long      : " + Float.SIZE);
+
+        // Char
+        char c = 'c';
+        System.out.println("====CHAR===");
+        System.out.println("nilai Character c   = " + c);
+        System.out.println("Besar Character     : " + Character.SIZE);
+
+        // Boolean
+        boolean fal = false;
+        System.out.println("====BOOLEAN===");
+        System.out.println("nilai Boolean fal   = " + fal);
+        System.out.println("Besar Boolean       : " + Boolean.TRUE);
+        System.out.println("Besar Boolean       : " + Boolean.FALSE);
+        
+        
+
+        
+    }
+}
+
